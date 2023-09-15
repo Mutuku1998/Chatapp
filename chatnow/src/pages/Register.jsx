6befebd9b795bcae183f6b50b4b86396a4af6e99
mutuku1998 <martinmutuku1998@gmail.com>
2023-09-15 11:04:06 +0300

    Update Register.jsx

--- chatnow/src/pages/Register.jsx
+++ chatnow/src/pages/Register.jsx
@@ -1,77 +1,88 @@
-import React, { useState } from 'react'
-import '../styles/styles.scss'
-import {  createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import {auth , db, storage} from '../Firebase';
-import { doc, setDoc } from "firebase/firestore"; 
+import React, { useState } from 'react';
+import '../styles/styles.scss';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { auth, db, storage } from '../Firebase';
+import { doc, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+
 const Register = () => {
-  const [err,setErr] = useState(false)
-  const handleSubmit =  async  (e)=>{
-   e.preventDefault() 
- const name = e.target[0].value;
- const email = e.target[1].value;
- const password = e.target[2].value;
- const file = e.target[3].files[0];
- 
-try{
-  const res =  await createUserWithEmailAndPassword(auth, email, password)
-const storageRef = ref(storage, name);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
-const uploadTask = uploadBytesResumable(storageRef, file); 
-uploadTask.on( 
-  (error) => {
-setErr(true);
-  }, 
-  () => {
-    
-    getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
-await updateProfile(res.user,{
-  name,
-  photoURL:downloadURL,
-});
-await setDoc(doc(db,'users', res.user.uid),{
-  uid:res.user.uid,
-  name,
-  email,
-  photoURL: downloadURL
-})
-    });
-   
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-);
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
 
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
 
-}
-catch(err){
-setErr(true)
-}
+      const storageRef = ref(storage, name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
 
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          // Progress tracking if needed
+        },
+        (error) => {
+          console.error(error);
+          setError(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(user, {
+              displayName: name,
+              photoURL: downloadURL,
+            });
 
+            const userData = {
+              uid: user.uid,
+              name,
+              email,
+              photoURL: downloadURL,
+            };
 
+            await setDoc(doc(db, 'users', user.uid), userData);
+            await setDoc(doc(db, 'userChats', user.uid), {});
+
+            navigate('/');
+          });
+        }
+      );
+    } catch (err) {
+      console.error(err);
+      setError(true);
+    }
   };
-  return (
-    <div className='formcontainer'>
-<div className='formwrapper'>
-    <span className='logo'>Chat with Martin</span>
-    <span className='title'>Register</span>
-    <form onSubmit={handleSubmit}> 
-        <input type='text' placeholder='name'/>
-        <input type='email' placeholder='email'/>
-        <input type='password' placeholder='password'/>
-        <input type='file' id='file'
-        style={{display:'none'}}/>
-        <label htmlFor='file'>
-        <i class="ri-image-add-fill"></i>
-          Add profile
-        </label>
-        <button> Sign Up</button>
-        {err && <span>Something went wrong.Please try again</span>}
-    </form>
-    <p>You do have an account ? Login </p>
-</div>
-    </div>
-  )
-}
 
-export default Register
+  return (
+    <div className="formcontainer">
+      <div className="formwrapper">
+        <span className="logo">Chat with Martin</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="file" id="file" style={{ display: 'none' }} />
+          <label htmlFor="file">
+            <i className="ri-image-add-fill"></i> Add profile
+          </label>
+          <button>Sign Up</button>
+          {error && <span>Something went wrong. Please try again.</span>}
+        </form>
+        <p>
+          You already have an account? <a href="/login">Login</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
